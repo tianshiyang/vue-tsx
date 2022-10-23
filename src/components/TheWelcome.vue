7eec152a8ee1d75908cae4cbc3a9a0e695557f89
田世洋 <tianshiyang@honganrobots.com>
2022-10-23 16:08:04 +0800

    feat: 改造icon下的vue文件

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import WelcomeItem from "./WelcomeItem.js";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
-import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
+import DocumentationIcon from "./icons/IconDocumentation.js";
+import ToolingIcon from "./icons/IconTooling.js";
+import EcosystemIcon from "./icons/IconEcosystem.js";
+import CommunityIcon from "./icons/IconCommunity.js";
+import SupportIcon from "./icons/IconSupport.js";
 </script>
 
 <template>
